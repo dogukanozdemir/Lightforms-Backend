83bbf65c08d95e05055ab20f63ab2cf23c23328e
Harun Selman Karakaş <harunselmankarakas@gmail.com>
2023-02-17 18:09:06 +0300

    initial

---
+++ src/main/java/com/forms/lightweight/lightweight/LightweightApplication.java
@@ -0,0 +1,13 @@
+package com.forms.lightweight.lightweight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightweightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightweightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forms/lightweight/lightweight/LightweightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forms.lightweight.lightweight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightweightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
