4d5d0741f9e41a5f9e7a02955aed171f3fb8f502
Doğukan Özdemir <dogu757@gmail.com>
2023-06-06 02:27:16 +0300

    fixed add question

--- src/main/java/com/forms/lightweight/lightweight/form/question/dto/AddQuestionResponseDto.java
+++ src/main/java/com/forms/lightweight/lightweight/form/question/dto/AddQuestionResponseDto.java
@@ -2,11 +2,13 @@ package com.forms.lightweight.lightweight.form.question.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Data
 public class AddQuestionResponseDto {
 
     private Long id;
